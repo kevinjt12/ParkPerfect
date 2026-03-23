d796e913d83e1b930346ed20a39b39cf54345bad
Ryan Woodzell <ryanwoodzell@gmail.com>
2026-03-23 18:18:12 -0400

    update views.py

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from datetime import date
+from parking.services import get_lots
+from .services import calculate_statistics
+from .models import StatisticsReport
 
-# Create your views here.
+class StatisticsView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        start_date = request.query.params.get('start', str(date.today()))
+        end_date = request.query.params.get('end', str(date.today()))
+        lots = get_lots()
+        statistics = calculate_statistics(lots, start_date, end_date)
+
+        return Response({
+            'dateRange': f'{start_date} to {end_date}',
+            'statistics': statistics
+        })
